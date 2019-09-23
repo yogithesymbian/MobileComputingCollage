52ebe7b5dcafe614e05ea8fe9dc54f9cb94da797
Syahdananugrah24 <syahdananugrah24@gmail.com>
2019-09-23 14:50:58 +0800

    test

--- app/src/main/java/com/scodeid/mobilecomputingcollage/MainCalculatorActivity.java
+++ app/src/main/java/com/scodeid/mobilecomputingcollage/MainCalculatorActivity.java
@@ -9,5 +9,7 @@ public class MainCalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_calculator);
+		
+		// test
     }
 }
