d2e04e27ae7e87cd0a3f9fd511d44b1134f11e1e
yogithesymbian <yogirenbox33@gmail.com>
2019-09-23 12:04:23 +0800

    Initial commit

---
+++ app/src/main/java/com/scodeid/mobilecomputingcollage/MainCalculatorActivity.java
@@ -0,0 +1,13 @@
+package com.scodeid.mobilecomputingcollage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainCalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_calculator);
+    }
+}
