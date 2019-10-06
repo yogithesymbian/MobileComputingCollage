b38928d15a70e60c68a569fef52fe25eff0c1846
yogithesymbian <yogirenbox33@gmail.com>
2019-10-07 03:52:34 +0800

    sentuhan-akhir-kamus-3-bahasa

--- kamus/src/main/java/com/scodeid/kamus/TerjemahanActivity.java
+++ kamus/src/main/java/com/scodeid/kamus/TerjemahanActivity.java
@@ -13,60 +13,151 @@ import com.scodeid.kamus.helper.DatabaseHelper;
 
 public class TerjemahanActivity extends AppCompatActivity {
 
-    SQLiteDatabase db = null;
+    SQLiteDatabase sqLiteDatabase = null;
     Cursor kamusCursor = null;
-    EditText textInggris;
-    EditText textIndonesia;
-    EditText textBanjar;
-    DatabaseHelper datakamus = null;
-    Button btnTerjemah;
+    private EditText textSpanyol;
+    private EditText textIndonesia;
+    private EditText textBali;
+    DatabaseHelper databaseHelper = null;
 
-    public static final String INGGRIS = "inggris";
+    public static final String SPANYOL = "spanyol";
     public static final String INDONESIA = "indonesia";
-    public static final String BANJAR = "banjar";
+    public static final String BALI = "bali";
+    public String translateFrom = INDONESIA;
+    private String TAG_LOG = TerjemahanActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terjemahan);
 
-        datakamus = new DatabaseHelper(this);
-        db = datakamus.getWritableDatabase();
+        databaseHelper = new DatabaseHelper(this);
+        sqLiteDatabase = databaseHelper.getWritableDatabase();
         setContentView(R.layout.activity_terjemahan);
-        textInggris = findViewById(R.id.txtInggris);
-        textIndonesia = findViewById(R.id.txtIndonesia);
-        textBanjar = findViewById(R.id.txtBanjar);
 
-        btnTerjemah = findViewById(R.id.btn_terjemah);
+        textSpanyol = findViewById(R.id.txt_spanyol);
+        textIndonesia = findViewById(R.id.txt_indonesia);
+        textBali = findViewById(R.id.txt_bali);
+
+        Button btnIndoState = findViewById(R.id.btn_frm_indonesia);
+        Button btnSpanyolState = findViewById(R.id.btn_frm_spanyol);
+        Button btnBaliState = findViewById(R.id.btn_frm_bali);
+        final Button btnTerjemah = findViewById(R.id.btn_terjemahkan);
+
+        btnIndoState.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                translateFrom = INDONESIA;
+                btnTerjemah.setText(R.string.terjemahan_activity_indonesia);
+                Log.d(TAG_LOG,"1. Translate From" + translateFrom);
+                //visible | change position | hint text field from -> to result , later's
+            }
+        });
+        btnSpanyolState.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                translateFrom = SPANYOL;
+                btnTerjemah.setText(R.string.terjemahan_activity_spanyol);
+                Log.d(TAG_LOG,"2. Translate From" + translateFrom);
+                //visible | change position | hint text field from -> to result , later's
+            }
+        });
+        btnBaliState.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                translateFrom = BALI;
+                btnTerjemah.setText(R.string.terjemahan_activity_balinese);
+                Log.d(TAG_LOG,"3. Translate From" + translateFrom);
+                //visible | change position | hint text field from -> to result , later's
+            }
+        });
+
+
         btnTerjemah.setOnClickListener(new View.OnClickListener(
         ) {
             @Override
             public void onClick(View v) {
-                String bhsinggris = "";
-                String bhsbanjar = "";
-                String bhsindonesia = textIndonesia.getText().toString();
-                kamusCursor = db.rawQuery("SELECT _ID, INDONESIA, INGGRIS, BANJAR " + " FROM kamus WHERE INDONESIA= '"+ bhsindonesia +"' ORDER BY INDONESIA",null);
-                if (kamusCursor.moveToFirst()){
-                    for(; !kamusCursor.isAfterLast(); kamusCursor.moveToNext()){
-                        bhsinggris = kamusCursor.getString(2);
-                        bhsbanjar = kamusCursor.getString(3);
+                switch (translateFrom) {
+                    case INDONESIA: {
+
+                        String bhsSpanyol = "";
+                        String bhsBali = "";
+                        String bhsIndonesia = textIndonesia.getText().toString();
+                        letsTranslate(INDONESIA, bhsIndonesia, bhsBali, bhsSpanyol, 1);
+
+                        break;
                     }
-                }else{
-                    Toast.makeText(getBaseContext(), "Terjemahan tidak ditemukan",
-                            Toast.LENGTH_SHORT).show();
+                    case SPANYOL: {
+
+                        String bhsSpanyol = textSpanyol.getText().toString();
+                        String bhsBali = "";
+                        String bhsIndonesia = "";
+                        letsTranslate(SPANYOL, bhsSpanyol, bhsBali, bhsIndonesia, 2);
+
+                        break;
+                    }
+                    case BALI: {
+
+                        String bhsSpanyol = "";
+                        String bhsBali = textBali.getText().toString();
+                        String bhsIndonesia = "";
+                        letsTranslate(BALI, bhsBali, bhsSpanyol, bhsIndonesia, 3);
+
+                        break;
+                    }
                 }
-                textInggris.setText(bhsinggris);
-                textBanjar.setText(bhsbanjar);
+            }
+
+            private void letsTranslate(String fieldFrom, String valueTranslate, String index3, String index2, int stateFrom) {
+                kamusCursor = sqLiteDatabase.rawQuery("SELECT _ID, INDONESIA, SPANYOL, BALI " + " FROM kamus WHERE "+ fieldFrom +"= '"+ valueTranslate +"' ORDER BY INDONESIA",null);
+
+                if (stateFrom == 1){
+                    if (kamusCursor.moveToFirst()){
+                        for(; !kamusCursor.isAfterLast(); kamusCursor.moveToNext()){
+                            index2 = kamusCursor.getString(2); // SPANYOL
+                            index3 = kamusCursor.getString(3); // BALI
+                        }
+                    }else{
+                        Toast.makeText(getBaseContext(), "Terjemahan tidak ditemukan",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    textSpanyol.setText(index2);
+                    textBali.setText(index3);
+                }
+                else if(stateFrom == 2){
+                    if (kamusCursor.moveToFirst()){
+                        for(; !kamusCursor.isAfterLast(); kamusCursor.moveToNext()){
+                            index2 = kamusCursor.getString(1); // INDONESIA
+                            index3 = kamusCursor.getString(3); // BALI
+                        }
+                    }else{
+                        Toast.makeText(getBaseContext(), "Terjemahan tidak ditemukan",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    textIndonesia.setText(index2);
+                    textBali.setText(index3);
+                } else if(stateFrom == 3){
+                    if (kamusCursor.moveToFirst()){
+                        for(; !kamusCursor.isAfterLast(); kamusCursor.moveToNext()){
+                            index2 = kamusCursor.getString(1); // INDONESIA
+                            index3 = kamusCursor.getString(2); // SPANYOL
+                        }
+                    }else{
+                        Toast.makeText(getBaseContext(), "Terjemahan tidak ditemukan",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    textIndonesia.setText(index2);
+                    textSpanyol.setText(index3);
+                }
             }
         });
     }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
         try{
             kamusCursor.close();
-            db.close();
+            sqLiteDatabase.close();
         }catch (Exception e){
             e.printStackTrace();
         }
